a209d61be7cf98752b479f16ea8936eb99d986e5
phantware <phantmoney2011@gmail.com>
2019-11-13 11:30:51 +0100

    This is the continuation of the first part of API

---
+++ server.js
@@ -0,0 +1,28 @@
+// server.js
+import express from 'express';
+import dotenv from 'dotenv';
+import 'babel-polyfill';
+import ReflectionWithJsObject from './src/usingJSObject/controllers/Reflection';
+import ReflectionWithDB from './src/usingDB/controllers/Reflection';
+
+dotenv.config();
+const Reflection = process.env.TYPE === 'db' ? ReflectionWithDB : ReflectionWithJsObject;
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  return res.status(200).send({'message': 'YAY! Congratulations! Your first endpoint is working'});
+});
+
+
+// import Reflection from './src/controllers/Reflection';
+
+app.post('/api/v1/reflections', Reflection.create);
+app.get('/api/v1/reflections', Reflection.getAll);
+app.get('/api/v1/reflections/:id', Reflection.getOne);
+app.put('/api/v1/reflections/:id', Reflection.update);
+app.delete('/api/v1/reflections/:id', Reflection.delete);
+
+app.listen(3000)
+console.log('app running on port ', 3000);
